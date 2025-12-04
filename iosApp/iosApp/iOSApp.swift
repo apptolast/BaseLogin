986dbcc829e87dc337cd5a30320fa923635de18e
Alberto Hidalgo <hgarcia.alberto@gmail.com>
2025-12-04 19:57:50 +0000

    Feat: Add Firebase App Check for iOS
    This commit introduces Firebase App Check to the iOS application.
    
    It includes:
    - Adding the `FirebaseAppCheck` dependency in the Podfile.
    - Configuring a debug provider for App Check during application launch.

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,9 +1,14 @@
 import SwiftUI
 import FirebaseCore
+import FirebaseAppCheck
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+
+        let providerFactory = AppCheckDebugProviderFactory()
+        AppCheck.setAppCheckProviderFactory(providerFactory)
+
         FirebaseApp.configure()
 
         return true
